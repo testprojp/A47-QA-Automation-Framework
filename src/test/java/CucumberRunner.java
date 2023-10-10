ba91bea574de386c51cf118779207f6001b2e1f9
testprojp <james.j.patterson.jr@testpro.io>
2023-10-10 11:52:01 -0500

    updated code
    updated code

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -1,5 +1,4 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -25,7 +24,7 @@ public class CucumberRunner extends AbstractTestNGCucumberTests
     }
 
     @Test(dataProvider = "features")
-    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) throws Throwable {
+    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
         testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
     }
 
